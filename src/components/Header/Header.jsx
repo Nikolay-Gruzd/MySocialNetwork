6189d1caef9dce879b6ccfdd9b63723f0b1a5db0
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2021-07-21 14:33:49 +0800

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR34dyR4PdRqvfNi_QIruoGyjHihGJrL98VuA&usqp=CAU'/>
+        </header>
+    )
+}
+
+export default Header;
